72d2abb1caa5b9feb71fe307723541cd659df64c
Suriya-Prasad <prasad.suriya782@gmail.com>
2022-05-24 00:13:45 +0530

    Update leave_entry_superadmin.php

--- FrontEnd/leave_entry_superadmin.php
+++ FrontEnd/leave_entry_superadmin.php
@@ -45,7 +45,7 @@
 <?php
 function GetLeaveRequests(){
     $conn = connectDB();     
-    $query = "SELECT d.employeeID,CONCAT(e.first_name,' ',e.last_name)as employee_name,MIN(d.from_date) as join_date from employee as e, designation as d WHERE d.employeeID IN(select employeeID from designation where posting ='{$posting}' and to_date is null AND from_date is not null) AND posting = '{$posting}'  AND e.employeeID = d.employeeID GROUP BY employeeID ORDER BY join_date;";
+    $query = "SELECT d.employeeID,CONCAT(e.first_name,' ',e.last_name)as employee_name,MIN(d.from_date) as join_date from employee as e, designation as d WHERE d.employeeID IN(select employeeID from designation where posting ='{}' and to_date is null AND from_date is not null) AND posting = '{}'  AND e.employeeID = d.employeeID GROUP BY employeeID ORDER BY join_date;";
     if($result = mysqli_query( $conn, $query)){
         $returnVal = seniorityTable($result);
         mysqli_close($conn);
