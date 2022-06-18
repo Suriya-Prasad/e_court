e7834929651159d52d1d7e67c2021dda9020382f
Suriya-Prasad <prasad.suriya782@gmail.com>
2022-06-19 00:44:50 +0530

    Update complaint_history_user_admin.php

--- FrontEnd/complaint_history_user_admin.php
+++ FrontEnd/complaint_history_user_admin.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <?php
     include_once "navigation.php";
+    include_once "actions.php";
+    if(strcmp($_SESSION['employee_role'],"super admin")==0){
+        header("Location:home_attendance.php");
+    }
 ?>
 
 <html lang="en">
